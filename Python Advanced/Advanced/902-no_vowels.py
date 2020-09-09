8688679696f589a96272fa2cc46a1ceaa68edca7
krsivanov <krsivanov@gmail.com>
2020-09-09 18:17:59 +0300

    Update 902-no_vowels.py

--- Python Advanced/Advanced/902-no_vowels.py
+++ Python Advanced/Advanced/902-no_vowels.py
@@ -2,5 +2,5 @@ text = input()
 
 vowels = ['a', 'o', 'u', 'e', 'i']
 
-no_vowels_text = [ch for ch in text if ch not in vowels]
+no_vowels_text = [ch for ch in text if ch.lower() not in vowels]
 print(''.join(no_vowels_text))
