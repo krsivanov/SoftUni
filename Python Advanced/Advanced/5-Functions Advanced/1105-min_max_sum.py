ebee6a9c16d5806e0e67419c72619612599984f3
krsivanov <krsivanov@gmail.com>
2020-09-12 17:57:35 +0300

    Create 1105-min_max_sum.py

---
+++ Python Advanced/Advanced/5-Functions Advanced/1105-min_max_sum.py
@@ -0,0 +1,9 @@
+ll = [int(x) for x in input().split()]
+
+min_number= min(ll)
+max_number = max(ll)
+numbers_sum = sum(ll)
+
+print(f'Maximum number is: {min_number}')
+print(f'Minimum number is: {max_number}')
+print(f'The sum is:{numbers_sum}')
