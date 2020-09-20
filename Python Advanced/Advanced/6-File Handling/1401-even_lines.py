6fa11666e665176444bfea1985d44fd22f836e7a
krsivanov <krsivanov@gmail.com>
2020-09-20 09:52:43 +0300

    Update 1401-even_lines.py

--- Python Advanced/Advanced/6-File Handling/1401-even_lines.py
+++ Python Advanced/Advanced/6-File Handling/1401-even_lines.py
@@ -2,6 +2,6 @@ with open("text.txt", "w") as file:
     for idx, line in enumerate(file):
         if idx%2 == 0:
             for el in "-,.!?":
-                line = line.replace(el, @)
+                line = line.replace(el, "@")
             words = reversed(line.split())
             print(' '.join(words))
