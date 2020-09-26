78a7ab0ccab5385041bbee00a16206c85e667c20
krsivanov <krsivanov@gmail.com>
2020-09-26 18:35:05 +0300

    Update 2102-easter_bunny.py

--- Python Advanced/Advanced/10-Exam Preparation/2102-easter_bunny.py
+++ Python Advanced/Advanced/10-Exam Preparation/2102-easter_bunny.py
@@ -1,12 +1,13 @@
-def is_valid(number,size):
-    return 0<=number< size
+def is_valid(number, size):
+    return 0 <= number < size
+
 
 n = int(input())
 
 bunny_pos = ()
 best_sum = - 9999999999999999999
 best_dir = ""
-directions = {"up":(-1,0), "right": (0,1), "down": (1,0), "left": (0,-1)}
+directions = {"up": (-1, 0), "right": (0, 1), "down": (1, 0), "left": (0, -1)}
 field = []
 
 for row in range(n):
@@ -20,29 +21,37 @@ for direction in directions:
     row = bunny_pos[0]
     col = bunny_pos[1]
 
-    while is_valid(row, n) and is_valid(col,n):
+    row_changes = directions[direction][0]
+    col_changes = directions[direction][1]
+
+    if not is_valid(row + row_changes, n) or not is_valid(col + col_changes, n):
+        continue
+
+    while is_valid(row, n) and is_valid(col, n):
         current_cell = field[row][col]
-        if current_cell !="B" and current_cell!="X":
+        if current_cell != "B" and current_cell != "X":
             current_sum += int(current_cell)
         elif current_cell == "X":
             break
-        row += directions[direction][0]
-        col += directions[direction][1]
+        row += row_changes
+        col += col_changes
 
     if current_sum > best_sum:
         best_sum = current_sum
         best_dir = direction
 
 print(best_dir)
-row = bunny_pos[0] + direction[best_dir][0]
-col = bunny_pos[1] + direction[best_dir][1]
+row_changes = directions[best_dir][0]
+col_changes = directions[best_dir][1]
 
-while is_valid(row,n) and is_valid(col,n):
-    print([row,col])
+row = bunny_pos[0] + row_changes
+col = bunny_pos[1] + col_changes
+
+while is_valid(row, n) and is_valid(col, n):
+    if field[row][col] == "X":
+        break
+    print([row, col])
     row += directions[best_dir][0]
     col += directions[best_dir][1]
 
-
-
 print(best_sum)
-
