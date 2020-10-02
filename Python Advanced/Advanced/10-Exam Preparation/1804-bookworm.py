273fd742973cf92f323bd678e7514f4a78a1ac3b
krsivanov <krsivanov@gmail.com>
2020-10-02 18:15:54 +0300

    Update 1804-bookworm.py

--- Python Advanced/Advanced/10-Exam Preparation/1804-bookworm.py
+++ Python Advanced/Advanced/10-Exam Preparation/1804-bookworm.py
@@ -1,3 +1,8 @@
+def is_valid(pos,size):
+    return 0<=pos[0]<size and 0<=pos[1]<size
+
+
+
 inital_string = [x for x in input()]
 n = int(input())
 
@@ -13,13 +18,29 @@ directions = {
 for row in range(n):
     line = [x for x in input()]
     for col in range(n):
-        if line[col]== "p":
+        if line[col]== "P":
             player_pos = [row,col]
-        matrix.append(line)
+    matrix.append(line)
 
 m = int(input())
 
 for _ in range(m):
     command = input()
     dir_changes = directions[command]
-    
+    new_pos = [player_pos[0]+dir_changes[0], player_pos[1] + dir_changes[1]]
+
+    if is_valid(new_pos, n):
+        matrix[player_pos[0]][player_pos[1]]="-"
+        element = matrix[new_pos[0]][new_pos[1]]
+        if element!="-":
+            inital_string.append(element)
+        
+        player_pos = new_pos
+        matrix[player_pos[0]][player_pos[1]] ="P"
+
+    else:
+        if inital_string:
+            inital_string.pop()
+
+print("".join(inital_string))
+[print("".join(row)) for row in matrix]
