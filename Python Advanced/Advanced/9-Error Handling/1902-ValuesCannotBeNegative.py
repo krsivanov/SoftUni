3152aa12b55bc49a0fce2a5c4842994ca5eafd01
krsivanov <krsivanov@gmail.com>
2020-10-05 10:12:59 +0300

    Create 1902-ValuesCannotBeNegative.py

---
+++ Python Advanced/Advanced/9-Error Handling/1902-ValuesCannotBeNegative.py
@@ -0,0 +1,11 @@
+class ValueCannotBeNegativeError(Exception):
+    def __init__(self, value):
+        self.value = value
+        msg = f"Value {value} is negative"
+        super(ValueCannotBeNegativeError, self).__init__(msg)
+
+n = 5
+for _ in range(n):
+    x = int(input())
+    if x < 0:
+        raise ValueCannotBeNegativeError(x)
