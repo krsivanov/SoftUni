977b011a45169ad2c4fd278889003d5170b01014
krsivanov <krsivanov@gmail.com>
2020-10-07 18:12:49 +0300

    Update 2004-dating_app.py

--- Python Advanced/Advanced/10-Exam Preparation/2004-dating_app.py
+++ Python Advanced/Advanced/10-Exam Preparation/2004-dating_app.py
@@ -1,1 +1,20 @@
-afa
+from collections import deque
+
+males = [int(x) for x in input().split()]
+females = deque([int(x) for x in input().split()])
+matches = 0
+
+while males and females:
+    male = males[-1]
+    female = females[0]
+
+    if male == 0:
+        males.pop()
+        continue
+    elif females==0:
+        females.popleft()
+        continue
+    elif female==male:
+        females.popleft()
+        males.pop()
+        matches += 1
