3466f59800bb22484dab739640d9454f94969c67
krsivanov <krsivanov@gmail.com>
2020-10-08 12:07:18 +0300

    Update 2004-dating_app.py

--- Python Advanced/Advanced/10-Exam Preparation/2004-dating_app.py
+++ Python Advanced/Advanced/10-Exam Preparation/2004-dating_app.py
@@ -8,13 +8,27 @@ while males and females:
     male = males[-1]
     female = females[0]
 
-    if male == 0:
+    if male <= 0:
         males.pop()
-        continue
-    elif females==0:
+    elif female <= 0:
         females.popleft()
-        continue
-    elif female==male:
+    elif female % 25 == 0:
+        females.popleft()
+        if females:
+            females.popleft()
+    elif male % 25 == 0:
+        males.pop()
+        if males:
+            males.pop()
+    elif female == male:
         females.popleft()
         males.pop()
         matches += 1
+
+    else:
+        females.popleft()
+        males.append(males.pop() - 2)
+
+print(f"Matches: {matches}")
+print(f"Males left: {'none' if not males else ', '.join([str(x) for x in reversed(males)])}")
+print(f"Females left: {', '.join([str(x) for x in females]) if females else 'none'}")
