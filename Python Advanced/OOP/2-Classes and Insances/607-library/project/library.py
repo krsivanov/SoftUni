ab0a520191256eb023f751bed64e9f1280314462
Krasimir Venkov <masta_killaa@abv.bg>
2020-10-20 09:58:55 +0300

    607-library_exercise
    classes and instances

--- Python Advanced/OOP/2-Classes and Insances/607-library/project/library.py
+++ Python Advanced/OOP/2-Classes and Insances/607-library/project/library.py
@@ -1,10 +1,8 @@
-from collections import defaultdict
-
 class Library:
     def __init__(self):
         self.user_records = []
         self.books_available = {}
-        self.rented_books = defaultdict(dict)
+        self.rented_books = {}
 
     def add_user(self, user):
         if user in self.user_records:
@@ -16,11 +14,12 @@ class Library:
             return f"We could not find such user to remove!"
         self.user_records.remove(user)
 
-    def change_username(self, user_id:int, new_username):
+    def change_username(self, user_id: int, new_username):
         user_list = list(filter(lambda user: user.user_id == user_id, self.user_records))
         if user_list:
             if user_list[0].username == new_username:
-                return f"Please check again the provided username - it should be different than the username used so far!"
+                return f"Please check again the provided username - " \
+                       f"it should be different than the username used so far!"
             user_list[0].username = new_username
             return f"Username successfully changed to: {new_username} for userid: {user_id}"
         return f"There is no user with id = {user_id}!"
