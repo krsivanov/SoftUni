9f30667084113d7b25e1911416b557c151a4adfb
krsivanov <krsivanov@gmail.com>
2020-11-05 11:43:19 +0200

    Create 1302-instruments.py

---
+++ Python Advanced/OOP/6-Polymorphism and Magic Methods/1302-instruments.py
@@ -0,0 +1,14 @@
+class Piano:
+    def play(self):
+        print('playing the piano')
+
+def play_instrument(instrument):
+    return instrument.play()
+
+
+
+guitar = Guitar()
+play_instrument(guitar)
+
+piano = Piano()
+play_instrument(piano)
