5d6951e3c7a0730ea7e0a49d74a1365e1b37b7d4
krsivanov <krsivanov@gmail.com>
2020-11-07 17:05:24 +0200

    Create 1501-books.py

---
+++ Python Advanced/OOP/7-Solid/1501-books.py
@@ -0,0 +1,40 @@
+class Book:
+    def __init__(self, title, author, total_pages):
+        self.total_pages = total_pages
+        self.title = title
+        self.author = author
+        self.page = 0
+
+
+class Library:
+    def __init__(self, location):
+        self.location = location
+        self.books = []
+
+    def find_book(self, title):
+        try:
+            book = [b for b in self.books if b.title == title][0]
+            return book
+        except:
+            return "No such book"
+
+    def add_book(self, book):
+        self.books.append(book)
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+class Reader(Person):
+    def __init__(self, name, current_book):
+        Person.__init__(self, name)
+        self.current_book = current_book
+        self.current_page = None
+
+    def turn_page(self, page):
+        if self.current_page:
+            self.current_book += 1
+            return
+        self.current_page += 1
