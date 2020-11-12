b57b7512f8a851f93e02aca58f126af3fe60bcf9
krsivanov <krsivanov@gmail.com>
2020-11-12 20:53:41 +0200

    Update 1704-squares.py

--- Python Advanced/OOP/8-Iterators and Generators/1704-squares.py
+++ Python Advanced/OOP/8-Iterators and Generators/1704-squares.py
@@ -4,7 +4,20 @@ def squares(n):
         yield current_num ** 2
         current_num += 1
 
+class squares_iter:
+    def __init__(self,n):
+        self.n = n
+        self.current_num = 1
 
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current_num <= self.n:
+            temp = self.current_num
+            self.current_num += 1
+            return temp**2
+        raise StopIteration()
 
 
 print(list(squares(5)))
