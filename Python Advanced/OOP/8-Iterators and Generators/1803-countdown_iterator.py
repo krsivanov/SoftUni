5c5c15d2db4cd726ce7892572ab3cfcb2900a983
krsivanov <krsivanov@gmail.com>
2020-11-13 15:14:03 +0200

    Create 1803-countdown_iterator.py

---
+++ Python Advanced/OOP/8-Iterators and Generators/1803-countdown_iterator.py
@@ -0,0 +1,22 @@
+class countdown_iterator:
+    def __init__(self,count):
+        self.count = count
+        self.end_count = 0
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count >= self.end_count:
+            temp = self.count
+            self.count -= 1
+            return temp
+        raise StopIteration()
+
+
+
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")
+
