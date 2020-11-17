055ba3043fe6974fdc7e2516f1b825565d9e6ad0
krsivanov <krsivanov@gmail.com>
2020-11-17 20:57:21 +0200

    Create 2101-testworker.py

---
+++ Python Advanced/OOP/10-Testing/2101-testworker.py
@@ -0,0 +1,73 @@
+#ima greshka...
+
+import unittest
+
+from worker import Worker
+
+
+class TestWorker(unittest.TestCase):
+    def test_workerInit_whenCorrectNameSalaryAndEnergy_shouldBeInitialized(self):
+        name = "Worker name"
+        salary = 123
+        energy = 5
+        worker = Worker(name, salary , energy)
+        self.assertEqual(name , worker.name)
+        self.assertEqual(salary, worker.salary)
+        self.assertEqual(energy, worker.energy)
+        self.assertEqual(0 , worker.money)
+
+    def test_workerRest_shouldIncrementEnergy(self):
+        name = "Worker name"
+        salary = 123
+        energy = 5
+        worker = Worker(name, salary, energy)
+
+        worker.rest()
+
+        self.assertEqual(energy+1, worker.energy)
+
+
+    def test_workerWork_whenEnergyIs0_shouldRaiseException(self):
+        name = "Worker name"
+        salary = 123
+        energy = 0
+        worker = Worker(name, salary, energy)
+        with self.assertRaises(Exception) as context:
+            worker.work()
+
+        self.assertIsNotNone(context.exception)
+
+    def test_workerWork_whenEnergyIsAbove0_shouldIncreaseMoneyBySalary(self):
+        name = "Worker name"
+        salary = 123
+        energy = 0
+        worker = Worker(name, salary, energy)
+
+        worker.work()
+        self.assertEqual(salary, worker.money)
+        worker.work()
+        self.assertEqual(salary*2, worker.money)
+
+    def test_workerWork_whenEnergyIsAbove0_shouldDecreaseEnergy(self):
+        name = "Worker name"
+        salary = 123
+        energy = 0
+        worker = Worker(name, salary, energy)
+
+        worker.work()
+        self.assertEqual(energy - 1, worker.energy)
+
+    def test_workerGetInfo_shouldReturnCorrectString(self):
+        name = "Worker name"
+        salary = 123
+        energy = 0
+        worker = Worker(name, salary, energy)
+
+        expected = f'{name} has saved 0 money.'
+
+        actual = worker.get_info()
+        self.assertEqual(expected,actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
