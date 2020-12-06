b8b6ca2e2a34c19d084ab3e6cc36f8351900a0b4
krsivanov <krsivanov@gmail.com>
2020-12-06 15:08:53 +0200

    Create card_repository.py

---
+++ Python Advanced/OOP/13-Exam Preparation/Exam-02-04-2020/project/card/card_repository.py
@@ -0,0 +1,36 @@
+from project.card.card import Card
+
+
+class CardRepository:
+    def __init__(self):
+        self.count = 0
+        self.cards = []
+
+    def add(self, card:Card):
+        try:
+            c = [c for c in self.cards if c.name == card.name][0]
+            raise ValueError(f"Card {card.name} already exists!")
+        except IndexError:
+            self.cards.append(card)
+            self.count += 1
+
+    # Be careful - here the param player is a string not the Player object
+    def remove(self, card:str):
+        if card == "":
+            raise ValueError("Card cannot be an empty string!")
+        c = [c for c in self.cards if c.name == card][0]
+        self.cards.remove(c)
+        self.count -= 1
+
+    def find(self, name: str):
+        c = [c for c in self.cards if c.name == name][0]
+        return c
+
+
+# from project.card.magic_card import MagicCard
+#
+# p = MagicCard("test")
+# pr = CardRepository()
+# pr.add(p)
+# print(pr.count)
+# print(pr.find("test"))
