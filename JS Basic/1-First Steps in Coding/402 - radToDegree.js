c635a18a06a349e5725b49fe4aed1ef7efa09880
krsivanov <krsivanov@gmail.com>
2020-12-14 09:31:42 +0200

    Update 402 - radToDegree.js

--- JS Basic/1-First Steps in Coding/402 - radToDegree.js
+++ JS Basic/1-First Steps in Coding/402 - radToDegree.js
@@ -2,6 +2,7 @@ function convertor(radians){
     let pi = Math.PI;
     let fixed = pi.toFixed(2);
     let degrees = radians *180 / pi;
-
+    
     console.log(degrees.toFixed(2));
+    // console.log(degrees.ceil(degrees))
 }
