9d2ce23c29804d6ec5a24e5230dff62b3daf8a35
krsivanov <krsivanov@gmail.com>
2020-12-18 07:39:40 +0200

    Create 304 - largest_number.js

---
+++ JS Advanced/1 - Syntax, Functions, Statements/304 - largest_number.js
@@ -0,0 +1,7 @@
+function findMaxNumber(first, second, third){
+    let maxNumber = Math.max(first, second, third)
+
+    console.log(`The largest number is ${maxNumber}.`)
+}
+
+findMaxNumber(5,-3,16)
