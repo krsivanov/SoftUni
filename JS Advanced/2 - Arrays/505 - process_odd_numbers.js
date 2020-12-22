43379e58bce8883f87d09dd2e88035904db7da95
krsivanov <krsivanov@gmail.com>
2020-12-22 17:00:26 +0200

    Create 505 - process_odd_numbers.js

---
+++ JS Advanced/2 - Arrays/505 - process_odd_numbers.js
@@ -0,0 +1,12 @@
+function solve(numbers){
+    let result = numbers
+        .filter((x, i) => i % 2 != 0)
+        .map(x => x*2)
+        .reverse()
+        .join(' ');
+    console.log(result);
+}
+
+
+
+solve([10, 15, 20, 25]);
