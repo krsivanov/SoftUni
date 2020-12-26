b4bad347a378de9de108daa27c1d6af0ba4d0e21
krsivanov <krsivanov@gmail.com>
2020-12-26 12:43:54 +0200

    Update 606 - sort_array.js

--- JS Advanced/2 - Arrays/606 - sort_array.js
+++ JS Advanced/2 - Arrays/606 - sort_array.js
@@ -1,29 +1,24 @@
-function solve(input) {
-    const twoCriteriasSort = (curr, next) => curr.length -next.length || curr.localeCompare(next);
-    input.sort(twoCriteriasSort);
-    console.log(input.join('\n')); 
-}
-
-// solve(['alpha', 'gamma', 'beta']);
-// solve(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
-solve(['test', 'Deny', 'omen', 'Default']);
-
-
-
-
-
+//first solution
 function solve(input) {
 
     input.sort((current, next) => {
         if (current.length > next.length){
             return 1;
         } else if (current.length===next.length){
-            return current > next;
+            return current.localeCompare(next);
         } else {
             return -1;
         }
     }).forEach(e => console.log(e));
     }
 
+//second solution
+function solve(input) {
+    const twoCriteriasSort = (curr, next) => curr.length -next.length || curr.localeCompare(next);
+    input.sort(twoCriteriasSort);
+    console.log(input.join('\n')); 
+}
 
-////////////////////////not working/////////// 
+solve(['alpha', 'gamma', 'beta']);
+solve(['Isacc', 'Theodor', 'Jack', 'Harrison', 'George']);
+solve(['test', 'Deny', 'omen', 'Default']);
