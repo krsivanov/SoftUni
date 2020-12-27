819db051200ca062f479b3f70eccac0f7ab29194
krsivanov <krsivanov@gmail.com>
2020-12-27 22:02:55 +0200

    Update 607 - tic_tac_toe.js

--- JS Advanced/2 - Arrays/607 - tic_tac_toe.js
+++ JS Advanced/2 - Arrays/607 - tic_tac_toe.js
@@ -1,3 +1,5 @@
+
+// unsolved
 function solve(input){
 
     let dashboard = [
@@ -46,3 +48,28 @@ solve(["0 1",
 "2 1",
 "2 2",
 "0 0"]);
+
+
+
+// function from collegue
+
+  function checkForWinner(currentBoard, sign) {
+        let isWinner = false;
+        for (let i = 0; i < 3; i++) {
+            if (currentBoard[i][0] === sign && currentBoard[i][1] === sign && currentBoard[i][2] === sign) {
+                isWinner = true;
+                break;
+            }
+            if (currentBoard[0][i] === sign && currentBoard[1][i] === sign && currentBoard[2][i] === sign) {
+                isWinner = true;
+                break;
+            }
+        }
+        if (!isWinner) {
+            if ((currentBoard[0][0] === sign && currentBoard[1][1] === sign && currentBoard[2][2] === sign) ||
+                (currentBoard[2][0] === sign && currentBoard[1][1] === sign && currentBoard[0][2] === sign)) {
+                isWinner = true;
+            }
+        }
+        return isWinner;
+    }
