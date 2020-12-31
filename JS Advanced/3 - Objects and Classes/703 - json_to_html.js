38f20ab43fc8a70ea6ddb2b7fa2576a1683c80db
krsivanov <krsivanov@gmail.com>
2020-12-31 09:57:54 +0200

    Update and rename 803 - json_to_html.js to 703 - json_to_html.js

--- JS Advanced/3 - Objects and Classes/703 - json_to_html.js
+++ JS Advanced/3 - Objects and Classes/703 - json_to_html.js
@@ -4,16 +4,16 @@ function solve(input){
 
     let first = students[0];
     let html = '<table>';
-    html += `<tr>${Object.keys(first).map(x => `<th>${x}</th>`).join('')}</tr>`;
+    html += `<tr>${Object.keys(first).map(x => `<th>${x}</th>`)}</tr>`;
     students.forEach(student => {
-        html += '<tr>';
-        html += Object.values(student).map(x => `<td>${x}</td>`).join('');
-        html += '</tr>';
+        html += '<tr>'
+        html += Object.values(student).map(x => `<td>${x}</td>`).join('')
+        html+= '</tr>'
     });
     html += '</table>';
-    console.log(html);
+    return html;
 }
+
 
 solve(['[{"Name":"Tomatoes & Chips","Price":2.35},{"Name":"J&B Chocolate","Price":0.96}]']);
 // solve(['[{"Name":"Pesho <div>-a","Age":20,"City":"Sofia"},{"Name":"Gosho","Age":18,"City":"Plovdiv"},{"Name":"Angel","Age":18,"City":"Veliko Tarnovo"}]']);
-
