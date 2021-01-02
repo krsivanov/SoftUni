4de59818742d7239fd486d4b048230e35df8cce1
krsivanov <krsivanov@gmail.com>
2021-01-02 09:35:21 +0200

    Create 801 - heroic_inventor.js

---
+++ JS Advanced/3 - Objects and Classes/801 - heroic_inventor.js
@@ -0,0 +1,20 @@
+function solve(input) {
+    let heroes = [];
+
+    input.forEach(line => {
+        let [name,level,items] = line.split(' / ');
+        level = Number(level);
+        items = items ? items.split(', ') : [];
+
+        let hero = {name: name, level, items: items};
+        heroes.push(hero);
+    });
+    console.log(JSON.stringify(heroes));
+
+}
+
+
+solve(['Jake / 1000 / Gauss, HolidayGrenade']);
+solve(['Isacc / 25 / Apple, GravityGun',
+    'Derek / 12 / BarrelVest, DestructionSword',
+    'Hes / 1 / Desolator, Sentinel, Antara']);
