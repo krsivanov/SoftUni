5ec734554cb9f7f3456253f7324fecc4e7a270e3
krsivanov <krsivanov@gmail.com>
2021-01-04 11:04:16 +0200

    Update 806 - system_components.js

--- JS Advanced/3 - Objects and Classes/806 - system_components.js
+++ JS Advanced/3 - Objects and Classes/806 - system_components.js
@@ -43,3 +43,30 @@ solve(['SULS | Main Site | Home Page',
 'Lambda | CoreC | C4',
 'Indice | Session | Default Storage',
 'Indice | Session | Default Security']);
+
+
+/////////////////////////////////////////other solution from collegue
+
+function solve(input) {
+    const register = {};
+    input.forEach(element => {
+        let [system, component, subcomponent] = element.split(' | ');
+        if (!register[system]) {
+            register[system] = {};
+        }
+        if (!register[system][component]) {
+            register[system][component] = [];
+        }
+        register[system][component].push(subcomponent);
+    });
+    const sortedSystems = Object.keys(register).sort((a, b) => Object.keys(register[b]).length - Object.keys(register[a]).length || a.localeCompare(b));
+
+    for (const system of sortedSystems) {
+        console.log(system);
+        const sortedComponents = Object.keys(register[system]).sort((a, b) => register[system][b].length - register[system][a].length);
+        for (const component of sortedComponents) {
+            console.log(`|||${component}`);
+            register[system][component].forEach(x => console.log(`||||||${x}`));
+        }
+    }
+}
