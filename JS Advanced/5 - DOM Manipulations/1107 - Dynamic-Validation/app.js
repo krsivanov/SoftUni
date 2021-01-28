8d596e67d079601648ecffe2ddab7b9cce4366ef
krsivanov <krsivanov@gmail.com>
2021-01-28 15:01:38 +0200

    Add files via upload

---
+++ JS Advanced/5 - DOM Manipulations/1107 - Dynamic-Validation/app.js
@@ -0,0 +1,17 @@
+function validate() {
+    let inputElement = document.getElementById('email');
+
+    inputElement.addEventListener('change', e => {
+        let currentValue = e.currentTarget.value;
+
+        let pattern = /[a-z]+@+[a-z]+.[a-z]/;
+
+        if (pattern.test(currentValue)) {
+            e.currentTarget.classList.remove('error');
+        } else {
+            e.currentTarget.classList.add('error');
+
+        }
+
+    })
+}
