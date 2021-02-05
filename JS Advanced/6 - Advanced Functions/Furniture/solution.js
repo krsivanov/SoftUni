04b88132519a830e3a2c165d701bfa0b9ad3f32f
krsivanov <krsivanov@gmail.com>
2021-02-05 15:14:47 +0200

    Update solution.js

--- JS Advanced/6 - Advanced Functions/Furniture/solution.js
+++ JS Advanced/6 - Advanced Functions/Furniture/solution.js
@@ -1,46 +1,68 @@
 function solve() {
-  const [input, output] = [...document.querySelectorAll('textarea')];
-  const table = document.querySelector('table.table tbody');
-  const [generateBtn, buyBtn] = [...document.querySelectorAll('button')];
+    const [input, output] = [...document.querySelectorAll('textarea')];
+    const table = document.querySelector('table.table tbody');
+    const [generateBtn, buyBtn] = [...document.querySelectorAll('button')];
 
-  const furnitureArray = JSON.parse(input.value.toString().trim());
+    const furniture = [];
 
-  generateBtn.addEventListener('click', () => {
-    table.innerHTML = '';
-    furnitureArray.forEach(f => table.appendChild(createRow(f)));  
-  });
-
-  buyBtn.addEventListener('click', () => {
-
-  });
-
-  function createRow(data){
-    const img = e('img');
-    img.src = data.img;
-
-    const check = e('input');
-    check.type = 'checkbox';
-
-    return e('tr',
-     e('td',img),
-     e('td',data.name),
-     e('td',data.price),
-     e('td',data.decFactor),
-     e('td',check)
-     );
-  }
-
-  function e(type, ...content){
-    const result = document.createElement(type);
-    
-    content.forEach(e => {
-      if (typeof e =='string'){
-        const nude = document.createTextNode(e);
-        result.appendChild(node);
-      } else {
-        result.appendChild(e);
-      }
+    generateBtn.addEventListener('click', () => {
+        const furnitureArray = JSON.parse(input.value.trim());
+        table.innerHTML = '';
+        furnitureArray.forEach(f => {
+            const item = createRow(f);
+            furniture.push(item);
+            table.appendChild(item.element);
+        });
     });
-    return result;
-  }
+
+    buyBtn.addEventListener('click', () => {
+        furniture.forEach(f => console.log(f.getValues().name, f.isChecked()));
+    });
+
+    const td = e.bind(null, 'td');
+
+    function createRow(data) {
+        const img = e('img');
+        img.src = data.img;
+
+        const check = e('input');
+        check.type = 'checkbox';
+
+        const element = e('tr',
+            td(img),
+            td(e('p', data.name)),
+            td(e('p', data.price)),
+            td(e('p', data.decFactor)),
+            td(check)
+        );
+
+        return {
+            element,
+            isChecked,
+            getValues
+        };
+
+        function isChecked() {
+            return check.checked;
+        }
+
+        function getValues() {
+            return data;
+        }
+    }
+
+    function e(type, ...content) {
+        const result = document.createElement(type);
+
+        content.forEach(e => {
+            if (typeof e == 'string') {
+                const node = document.createTextNode(e);
+                result.appendChild(node);
+            } else {
+                result.appendChild(e);
+            }
+        });
+
+        return result;
+    }
 }
