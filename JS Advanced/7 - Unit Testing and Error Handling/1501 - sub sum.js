2d6b198684db0cf85d9dab90d326b09573d2958e
Krasimir Venkov <krsivanov@gmail.com>
2021-02-09 13:55:59 +0200

    сс

--- JS Advanced/7 - Unit Testing and Error Handling/1501 - sub sum.js
+++ JS Advanced/7 - Unit Testing and Error Handling/1501 - sub sum.js
@@ -18,4 +18,4 @@ console.log(subSum([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1));
 console.log(subSum([10, 20, 30, 40, 50, 60], 3, 300));
 console.log(subSum([10, 'twenty', 30, 40], 0, 2));
 console.log(subSum([],1,2));
-console.log(subSum('text',0,2));s
+console.log(subSum('text',0,2));
