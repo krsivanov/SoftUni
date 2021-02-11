dd1ff70eaa2bad9a31df86ade771bc8f382a8a26
Krasimir Venkov <krsivanov@gmail.com>
2021-02-11 13:17:21 +0200

    c

---
+++ JS Advanced/7 - Unit Testing and Error Handling/1603 - char lookup.js
@@ -0,0 +1,21 @@
+const describe = require('mocha').describe;
+const assert = requiere('chai').assert;
+
+function lookupChar(string, index) {
+    if (typeof(string) !== 'string' || !Number.isInteger(index)){
+        return undefined;
+    }
+    if (string.length <= index || index <0) {
+        return 'Incorrect index';
+    }
+
+    return string.charAt(index);
+}
+
+describe('charLookup', () => {
+    it('', () =>{
+        assert.isUndefined(lookupChar(1,1));
+        assert.isUndefined(lookupChar('abc',1.2));
+        assert.isUndefined(lookupChar('abc','a'));
+    });
+});
