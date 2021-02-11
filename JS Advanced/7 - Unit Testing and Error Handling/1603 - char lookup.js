cc5df78fc2028b21147207a81d3c8c7f3e8cea30
Krasimir Venkov <krsivanov@gmail.com>
2021-02-11 22:28:02 +0200

    .

--- JS Advanced/7 - Unit Testing and Error Handling/1603 - char lookup.js
+++ JS Advanced/7 - Unit Testing and Error Handling/1603 - char lookup.js
@@ -18,4 +18,13 @@ describe('charLookup', () => {
         assert.isUndefined(lookupChar('abc',1.2));
         assert.isUndefined(lookupChar('abc','a'));
     });
+
+    it('', () => {
+        assert.equal(lookupChar('abv', -1), 'Incorrect index');
+        assert.equal(lookupChar('abv', 4), 'Incorrect index');
+    });
+    it('', () => {
+        assert.equal(lookupChar('abv', 0), "a");
+        assert.equal(lookupChar('abv',2),"v");
+    });
 });
---
+++ JS Advanced/7 - Unit Testing and Error Handling/1604 - math enforcer.js
@@ -0,0 +1,71 @@
+const { assert } = require("chai");
+
+let mathEnforcer = {
+    addFive: function (num) {
+        if (typeof(num) !== 'number') {
+            return undefined;
+        }
+        return num + 5;
+    },
+    subtractTen: function (num) {
+        if (typeof(num) !== 'number') {
+            return undefined;
+        }
+        return num - 10;
+    },
+    sum: function (num1, num2) {
+        if (typeof(num1) !== 'number' || typeof(num2) !== 'number') {
+            return undefined;
+        }
+        return num1 + num2;
+    }
+};
+
+
+describe ('mathEnforcer', () => {
+    describe('add', () => {
+        it('is not number', () => {
+            assert.isUndefined(mathEnforcer.addFive('a'));
+            assert.isUndefined(mathEnforcer.addFive(undefined));
+            assert.isNaN(mathEnforcer.addFive(NaN));
+        });
+
+        it('add properly', () => {
+            assert.equal(mathEnforcer.addFive(0),5);
+            assert.equal(mathEnforcer.addFive(-5),0);
+            assert.equal(mathEnforcer.addFive(1.2),6.2);
+            assert.equal(mathEnforcer.addFive(1),6);
+        });
+    });
+
+    describe('subtract', () => {
+        it('is not number', ()=>{
+            assert.isUndefined(mathEnforcer.subtractTen('a'));
+            assert.isUndefined(mathEnforcer.subtractTen(undefined));
+            assert.isNaN(mathEnforcer.subtractTen(NaN));
+        });
+        it('subtract properly', () => {
+            assert.equal(mathEnforcer.subtractTen(0),-10);
+            assert.equal(mathEnforcer.subtractTen(-5),-15);
+            assert.equal(mathEnforcer.subtractTen(10.2),0.1999999999999993);
+            assert.equal(mathEnforcer.subtractTen(1),-9);
+            assert.equal(mathEnforcer.subtractTen(10),0); 
+            assert.equal(mathEnforcer.subtractTen(20),10); 
+        });
+    });
+
+    describe('sum', () => {
+        it('is not number', () => {
+            assert.isUndefined(mathEnforcer.sum('1',2));
+            assert.isUndefined(mathEnforcer.sum(1,'2'));
+        });
+        it('proper tests', () => {
+            assert.equal(mathEnforcer.sum(1,1),2);
+            assert.equal(mathEnforcer.sum(1,-1),0);
+            assert.equal(mathEnforcer.sum(1,0),1);
+            assert.equal(mathEnforcer.sum(-1,-1),-2);
+            assert.equal(mathEnforcer.sum(-1.2,-1.3),-2.5);
+            assert.equal(mathEnforcer.sum(1.3,-1.2),0.10000000000000009);
+        });
+    });
+});
