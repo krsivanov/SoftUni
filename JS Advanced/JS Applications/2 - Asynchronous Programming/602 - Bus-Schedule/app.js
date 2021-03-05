c088f0a020ca989c87e399a36003b79d8d59796d
Krasimir Venkov <krsivanov@gmail.com>
2021-03-05 22:19:43 +0200

    async exercise

--- JS Advanced/JS Applications/2 - Asynchronous Programming/602 - Bus-Schedule/app.js
+++ JS Advanced/JS Applications/2 - Asynchronous Programming/602 - Bus-Schedule/app.js
@@ -1,11 +1,30 @@
 function solve() {
+    const departBtn = document.getElementById("depart");
+    const arriveBtn = document.getElementById('arrive');
+    const banner = document.querySelector('#info span ');
 
-    function depart() {
-        console.log('Depart TODO...');
+    let stop = {
+        next: 'depot'
+    };
+
+async function depart() {
+        const url = 'http://localhost:3030/jsonstore/bus/schedule/' + stop.next;
+        const response = await fetch(url);
+        const data = await response.json();
+
+        stop = data;
+
+        banner.textContent = `Next stop ${stop.name}`;
+
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        banner.textContent = `Arriving at ${stop.name}`
+
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
 
     return {
