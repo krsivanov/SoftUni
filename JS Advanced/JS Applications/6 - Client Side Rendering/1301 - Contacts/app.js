65d03bfa421b1f8c06e7be1e725d7c6940880ccb
Krasimir Venkov <krsivanov@gmail.com>
2021-03-26 19:18:35 +0200

    14

---
+++ JS Advanced/JS Applications/6 - Client Side Rendering/1301 - Contacts/app.js
@@ -0,0 +1,35 @@
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+import { contacts } from "./contacts.js";
+const cardTemplate = (data) => html`
+     <div class="contact card">
+            <div>
+                <i class="far fa-user-circle gravatar"></i>
+            </div>
+            <div class="info">
+                <h2>Name: ${data.name}</h2>
+                <button @click=${onClick} class="detailsBtn">Details</button>
+                <div class="details" id=${data.id}>
+                    <p>Phone number: ${data.phoneNumber}</p>
+                    <p>Email: ${data.email}</p>
+                </div>
+            </div>
+        </div>
+`;
+
+const container = document.getElementById('contacts');
+
+const result = contacts.map(cardTemplate);
+
+render(result, container);
+
+
+function onClick(event){
+    const element = event.target.parentNode;
+    const currentStyle = element.querySelector('.details').style.display;
+    if (currentStyle == 'block'){
+        element.querySelector('.details').style.display = 'none';
+    } else {
+        element.querySelector('.details').style.display = 'block';
+    }
+
+}
---
+++ JS Advanced/JS Applications/6 - Client Side Rendering/1406 - Book-Library/data.js
@@ -0,0 +1,48 @@
+async function requet(url, options) {
+    const response = await fetch(url,options);
+    const data = await response.json();
+
+    return data;
+}
+
+const host = 'http://localhost:3030/jsonstore/collections/books';
+
+async function getAllBooks() {
+    return Object
+                .entries(await requet(host))
+                .map(([k, v]) => Object.assign(v, { _id: k}))
+}
+
+async function getBookById(id) {
+    return await requet(host + '/' + id);
+}
+
+async function createBook(book) {
+    return await request(host, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(book)
+    });
+}
+
+async function updateBook(id, book) {
+    return await request(host + '/' + id, {
+        method: 'put',
+        headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify(book)
+    });
+}
+
+async function deleteBook(id,book) {
+    return await request(host + '/' + id, {
+        method: 'delete',
+    });
+}
+
+export {
+    getAllBooks,
+    getBookById,
+    createBook,
+    updateBook,
+    deleteBook
+};
