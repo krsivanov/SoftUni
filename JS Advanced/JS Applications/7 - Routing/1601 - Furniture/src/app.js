55fffff850661ed815d19d82d4b060dfcc9444ab
Krasimir Venkov <krsivanov@gmail.com>
2021-03-30 22:42:30 +0300

    1

---
+++ JS Advanced/JS Applications/7 - Routing/1601 - Furniture/src/app.js
@@ -0,0 +1,32 @@
+import page from '../node_modules/page/page.mjs'
+import { render } from "../node_modules/lit-html/lit-html.js";
+
+import { dashboardPage } from "./views/dashboard.js";
+import { createPage } from "./views/create.js";
+import { detailsPage } from "./views/details.js";
+import { editPage } from "./views/edit.js";
+import { registerPage } from "./views/register.js";
+import { loginPage } from "./views/login.js";
+import { myPage } from "./views/myFurniture.js";
+
+import * as api from './api/data.js'
+
+window.api = api
+
+const main = document.querySelector('.container');
+
+page('/', renderMiddleware, dashboardPage);
+// page('/dashboard', renderMiddleware, dashboardPage);
+page('/my-furniture', renderMiddleware, myPage);
+page('/details/:id', renderMiddleware, detailsPage);
+page('/create', renderMiddleware, createPage);
+page('/edit/:id', renderMiddleware, editPage);
+page('/register', renderMiddleware, registerPage);
+page('/login', renderMiddleware, loginPage);
+
+page.start();
+
+function renderMiddleware(ctx, next) {
+    ctx.render = (content) => render(content, main);
+    next();
+}
